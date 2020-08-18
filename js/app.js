b4274dc82f8dac3ed0343b8121b2cbca28c06d8d
Radwa Saleh <radwa.saleh@pixelogicmedia.com>
2020-08-18 16:19:19 +0200

    - Using arrow functions. - Adding eslintrc rules. - Adding .gitignore file. - Updated the starter code comments

--- js/app.js
+++ js/app.js
@@ -3,14 +3,12 @@
  *
  */
 
-let sections = document.getElementsByTagName('section');
+const sections = document.getElementsByTagName('section');
 /**
- * End Global Variables
- * Start Helper Functions
- *
+ *Helper Functions
  */
 
-function isInViewport(element) {
+const isInViewport = (element) => {
     const rect = element.getBoundingClientRect();
     return (
         rect.top >= 0 &&
@@ -18,20 +16,18 @@ function isInViewport(element) {
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
-}
+};
 
-function removeActiveClassFromAllSections() {
+const removeActiveClassFromAllSections = () => {
     for (const section of sections)
         section.classList.remove('active');
-}
+};
 
 /**
- * End Helper Functions
- * Begin Main Functions
- *
+ * Main Functions
  */
 
-function generateDynNavMenu (){
+const generateDynNavMenu = () =>{
     for (let i = 0; i < sections.length; i++){
         const list = document.getElementById('navbar__list');
 
@@ -44,40 +40,28 @@ function generateDynNavMenu (){
         listLink.setAttribute('href', `#${sections[i].getAttribute('id')}`);
         listLink.classList.add('menu__link', 'menu__link:hover');
         listLink.addEventListener('click', function () {
-            sections[i].scrollTo({behavior: 'smooth', top: 100, left: 100,});
+            sections[i].scrollIntoView({behavior: "smooth",block: "end", inline: "nearest"});
             event.preventDefault();
         });
 
         listItem.appendChild(listLink);
         list.appendChild(listItem);
     }
-}
+};
 
-// build the nav
-generateDynNavMenu();
-
-// Add class 'active' to section when near top of viewport
-window.addEventListener('scroll', function setActiveClass() {
+const setSectionActiveClass = () => {
     for(const section of sections){
         if(isInViewport(section)){
             removeActiveClassFromAllSections();
             section.classList.add('active');
         }
     }
-});
+};
 
-// Scroll to anchor ID using scrollTO event
+// build the nav
+generateDynNavMenu();
 
+// Add class 'active' to section when near top of viewport
+window.addEventListener('scroll', setSectionActiveClass);
 
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
 
